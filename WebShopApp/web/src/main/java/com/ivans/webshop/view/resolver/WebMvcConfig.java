c6a0752f9a559cefa4cf8c7df35983b04fa43c65
Ivan Sardelić <ivan.ssardelic@gmail.com>
2019-05-16 19:54:25 +0200

    Index.jsp rendered

---
+++ WebShopApp/web/src/main/java/com/ivans/webshop/view/resolver/WebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.ivans.webshop.view.resolver;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class WebMvcConfig {
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
